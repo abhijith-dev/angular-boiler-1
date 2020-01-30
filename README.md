4a91edba8a03c35309da8afe5c2652f19a0af5c0
abhijith1998 <shettyabhijith930@gmail.com>
2020-01-30 11:26:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-9ljvme
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-9ljvme)
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import {RouterModule,Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component'
+
+const routes:Routes=[
+  {
+    path:"",
+    component:HomeComponent
+  },
+  {
+    path:"home",
+    component:HomeComponent
+  }
+
+]
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule,RouterModule.forRoot(routes)],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, HomeComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
